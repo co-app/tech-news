68da9ccc25345cc00f5c73bf8f1e6f13c42a77ef
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-04-13 15:10:06 +0900

    [ADD] test

--- src/auth/test/auth.spec.ts
+++ src/auth/test/auth.spec.ts
@@ -1,26 +1,57 @@
+import { handler } from '..'
+
 describe('auth unit test', () => {
   // describe('[TEST] login unit test', () => {})
 
   describe('[TEST] signup unit test', () => {
-    it('[TEST] signup handler => SUCCESS', (done) => {
-      // const auth = {
-      //   username: 'leedonggyu',
-      //   password: '12341234',
-      //   email: 'zkfmapf123@naver.com',
-      // }
-      done()
+    const e = {
+      httpMethod: 'GET',
+      body: null,
+      headers: { 'Content-Type': 'application/json' },
+    }
+
+    it('[TEST] signup handler => Invalid Email', async () => {
+      await handler({
+        httpMethod: 'GET',
+        body: JSON.stringify({ tag: 'join', username: 'test', email: 'test', password: 'test' }),
+        headers: { 'Content-Type': 'application/json' },
+      }).catch((e) => {
+        expect(e._tag).toBe('fail')
+        expect(e.error.message).toBe('"email" must be a valid email')
+      })
     })
 
-    it('[TEST] signup handler => Invalid Email', (done) => {
-      done()
+    it('[TEST] signup handler => Invalid Password', async () => {
+      await handler({
+        httpMethod: 'GET',
+        body: JSON.stringify({ tag: 'join', username: 'test', email: 'zkfmapf123@naver.com', password: '' }),
+        headers: { 'Content-Type': 'application/json' },
+      }).catch((e) => {
+        expect(e._tag).toBe('fail')
+        expect(e.error.message).toBe('"password" is not allowed to be empty')
+      })
     })
 
-    it('[TEST] signup handler => Invalid Password', (done) => {
-      done()
+    it('[TEST] signup handler => Invalid Username', async () => {
+      await handler({
+        httpMethod: 'GET',
+        body: JSON.stringify({ tag: 'join', username: '', email: 'test', password: '12341234' }),
+        headers: { 'Content-Type': 'application/json' },
+      }).catch((e) => {
+        expect(e._tag).toBe('fail')
+        expect(e.error.message).toBe('"username" is not allowed to be empty')
+      })
     })
 
-    it('[TEST] signup handler => Invalid Username', (done) => {
-      done()
+    it('[TEST] signup handler => SUCCESS', async () => {
+      const result = await handler({
+        httpMethod: 'GET',
+        body: JSON.stringify({ tag: 'join', username: 'test', email: 'zkfmapf123@naver.com', password: '12341234' }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+
+      expect(result.statusCode).toBe(200)
+      expect(JSON.parse(result.body).message).toBe('hello world')
     })
   })
 })
