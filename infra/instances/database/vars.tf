be639950b260a582976d6c189b75f36698d1c51c
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-04-30 23:53:46 +0900

    wip

--- infra/instances/database/vars.tf
+++ infra/instances/database/vars.tf
@@ -10,14 +10,7 @@ variable "AWS_SECRET_KEY" {
   
 }
 
-variable "AWS_VPC_ID" {
-  
-}
-
-variable "AWS_PRIVATE_SUBNET_ID" {
-  
-}
-
 variable "AWS_MYSQL_PASSWORD" {
-  
+    type = string
+    default = "1234"
 }
--- infra/modules/security-group/aurora/main.tf
+++ infra/modules/security-group/aurora/main.tf
@@ -1,14 +1,18 @@
+variable AWS_RDS_CIDR_BLOCK {}
+variable AWS_VPC_ID {}
+
 resource "aws_security_group" "tech_news_sg" {
   name_prefix = "tech_news"
+  vpc_id = var.AWS_VPC_ID
 
   ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.AWS_RDS_CIDR_BLOCK]
   }
 }
 
-output AWS_SECURITY_GROUP{
+output AWS_SECURITY_GROUP {
     value = aws_security_group.tech_news_sg
 }
